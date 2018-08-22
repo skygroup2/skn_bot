474a0cee85af55e53618e5ebbe5aae7e10014e79
king22 <henrytan.next@gmail.com>
2018-08-22 10:14:27 +0700

    skn bot sup

--- lib/skn_bot.ex
+++ lib/skn_bot.ex
@@ -1,4 +1,20 @@
+defmodule Skn.Bot.Sup do
+  use Supervisor
+  @name :skn_bot_sup
+  def start_link() do
+    Supervisor.start_link(__MODULE__, [], name: @name)
+  end
+
+  def init(_) do
+    children = [
+      worker(RWSerializer, []),
+    ]
+    supervise(children, strategy: :one_for_one)
+  end
+end
+
 defmodule Skn.Bot do
+
   def q_correlation_id(bottype, botid) do
     case bottype do
     :none -> botid

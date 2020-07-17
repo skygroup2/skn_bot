af4f5d8659f85890f4a05c116afa20491d92894f
king22 <henrytan.next@gmail.com>
2020-07-17 09:17:49 +0700

    reformat old code

--- lib/db/bot_session.ex
+++ lib/db/bot_session.ex
@@ -1,55 +1,43 @@
 defmodule Skn.DB.BotSession do
-    require Logger
-    def get(bot_id) do
-        case :ets.lookup(:bot_session, bot_id) do
-            [c|_] -> c
-            _ -> nil
-        end
-    end
+  require Logger
 
-    def get(bot_type, bot_id) do
-        case bot_type do
-        :farmer ->
-            get(bot_id)
-        :order ->
-            get(bot_id)
-        _ ->
-            get("AH_#{bot_id}")
-        end
+  def to_id(bot_type, bot_id) do
+    if bot_type in [:farmer, :order] do
+      bot_id
+    else
+      "AH_#{bot_id}"
     end
+  end
 
-    def set(bot_type, bot, data) do
-        case bot_type do
-        :farmer ->
-            set(bot, data)
-        :order ->
-            set(bot, data)
-        _ ->
-            set("AH_#{bot}", data)
-        end
+  def get(bot_id) do
+    case :ets.lookup(:bot_session, bot_id) do
+      [c] -> c
+      _ -> nil
     end
+  end
 
-    defp set(bot, data) do
-        :ets.insert :bot_session, {bot, data}
-    end
+  def get(bot_type, bot_id) do
+    get(to_id(bot_type, bot_id))
+  end
 
-    def delete(bot_type, bot_id) do
-        case bot_type do
-        :farmer ->
-            delete(bot_id)
-        :order ->
-            delete(bot_id)
-        _ ->
-            delete("AH_#{bot_id}")
-        end
-    end
+  def set(bot_type, bot_id, data) do
+    set(to_id(bot_type, bot_id), data)
+  end
 
-    defp delete(bot_id) do
-        case get(bot_id) do
-        {_, %{pid: pid}} when pid == self() ->
-            :ets.delete(:bot_session, bot_id)
-        _ ->
-            nil
-        end
+  defp set(bot_id, data) do
+    :ets.insert(:bot_session, {bot_id, data})
+  end
+
+  def delete(bot_type, bot_id) do
+    delete(to_id(bot_type, bot_id))
+  end
+
+  def delete(bot_id) do
+    case get(bot_id) do
+      {_, %{pid: pid}} when pid == self() ->
+        :ets.delete(:bot_session, bot_id)
+      _ ->
+        nil
     end
+  end
 end
